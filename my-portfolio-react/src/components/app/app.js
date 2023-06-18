2790885c9098f2ec361d639d21cca2f4fa0adecd
Dima <dmitrijs.visnakovs@inbox.lv>
2023-06-18 09:22:37 +0300

    add portfolio-block and contact-form

--- my-portfolio-react/src/components/app/app.js
+++ my-portfolio-react/src/components/app/app.js
@@ -1,8 +1,13 @@
-import './app.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import AboutBlock from '../about-block/about-block';
 import MySkill from '../my-skills/MySkill';
+import LatesProjects from '../lates-projects/LatesProjects';
+
+
+import './app.css';
+import ContactForm from '../contact-form/ContactForm';
+
 
 function App () {
     return (
@@ -14,6 +19,9 @@ function App () {
         <div className='block'>
             <AboutBlock/>
             <MySkill/>
+            <LatesProjects/>
+           <ContactForm/>
+           
         </div>
 
         <div className='foot'>
